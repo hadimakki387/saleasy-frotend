64ea8799a88960a8b71a8e6d042c7a7b0cd0aedb
hadimakki387 <hmakki387@gmail.com>
2024-08-10 23:27:29 +0300

    fixed rtk

--- src/components/core/features/index.ts
+++ src/components/core/features/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import LandingPage from "./landing/redux/redux";
+import { mainApi } from "../rtk-query";
 
 const reducers = combineReducers({
   landingPage: LandingPage,
+  [mainApi.reducerPath]: mainApi.reducer,
 });
 
 export default reducers;
--- src/components/core/features/landing/redux/rtk.ts
+++ src/components/core/features/landing/redux/rtk.ts
@@ -4,10 +4,6 @@ export const extendedApi = mainApi.injectEndpoints({
   endpoints: (build) => ({
     getPokemon: build.query<any, void>({
       query: () => "https://pokeapi.co/api/v2/pokemon/ditto",
-      transformResponse: (response) => {
-        console.log("this is the response", response);
-        return response;
-      },
     }),
     //just an example of mutations
     postPokemon: build.mutation({
