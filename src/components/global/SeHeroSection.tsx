d6680bf52ffeb58ce5f81463accc7a11e04a602a
hadimakki387 <hmakki387@gmail.com>
2024-09-09 12:24:12 +0300

    fixed landing

--- src/components/global/SeHeroSection.tsx
+++ src/components/global/SeHeroSection.tsx
@@ -16,7 +16,7 @@ const Banner: React.FC<BannerProps> = ({
   console.log(data.redText);
   return (
     <div className="item-nkw  w-full">
-      {fullBanner ? (
+      <div className="md:hidden">
         <div
           className="relative overflow-hidden h-[13rem]"
           style={{
@@ -46,8 +46,9 @@ const Banner: React.FC<BannerProps> = ({
             </a>
           </div>
         </div>
-      ) : (
-        <div>
+      </div>
+      <div className="max-md:hidden">
+        {fullBanner ? (
           <div
             className="relative overflow-hidden h-[13rem]"
             style={{
@@ -56,29 +57,61 @@ const Banner: React.FC<BannerProps> = ({
               backgroundPosition: "center",
             }}
           >
-            <div className="flex items-center justify-center gap-1 text-4xl font-[800] text-primary h-full">
-              <p className=" tracking-wider">{data.text1?.toUpperCase()}</p>
-              <h4 className="  my-4 leading-none text-error">
-                {data.redText?.toUpperCase()}
+            <div className="absolute top-0 left-8 z-10 h-full flex flex-col justify-center">
+              <p className="text-xs font-light tracking-wider">{data.text1}</p>
+              <h4 className="text-lg font-semibold my-4 leading-none">
+                {data.text2}
+                <br />
+                <span className="font-light text-red-500">{data.redText}</span>
               </h4>
-              <span className="">{data.text2?.toUpperCase()}</span>
-            </div>
-            <div className="absolute right-4 top-4 bottom-4  flex items-center">
-              <SeButton
-                label={data.link.title}
-                variant="outlined"
-                color="primary"
-                sx={{
-                  padding: "0.75rem 3rem",
-                  fontSize: "1rem",
-                  textTransform: "none",
-                }}
-                noBorder
-              />
+              <a
+                href={data.link.target}
+                className="text-gray-800 font-semibold inline-flex items-center relative pb-1 hover:text-gray-800"
+              >
+                {data.link.title}
+                <svg
+                  className="w-4 h-4 inline-block fill-current ml-2 transition-transform"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" />
+                </svg>
+              </a>
             </div>
           </div>
-        </div>
-      )}
+        ) : (
+          <div>
+            <div
+              className="relative overflow-hidden h-[13rem]"
+              style={{
+                backgroundImage: `url('${imageUrl}')`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
+              <div className="flex items-center justify-center gap-1 text-4xl font-[800] text-primary h-full">
+                <p className=" tracking-wider">{data.text1?.toUpperCase()}</p>
+                <h4 className="  my-4 leading-none text-error">
+                  {data.redText?.toUpperCase()}
+                </h4>
+                <span className="">{data.text2?.toUpperCase()}</span>
+              </div>
+              <div className="absolute right-4 top-4 bottom-4  flex items-center">
+                <SeButton
+                  label={data.link.title}
+                  variant="outlined"
+                  color="primary"
+                  sx={{
+                    padding: "0.75rem 3rem",
+                    fontSize: "1rem",
+                    textTransform: "none",
+                  }}
+                  noBorder
+                />
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
