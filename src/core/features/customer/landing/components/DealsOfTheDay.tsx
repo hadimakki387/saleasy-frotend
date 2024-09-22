f65fbdf054c5d7089c0d63b962a3b86acc30efc7
hadi-makki <165915385+hadi-makki@users.noreply.github.com>
2024-09-22 22:21:03 +0300

    WIP

--- src/core/features/customer/landing/components/DealsOfTheDay.tsx
+++ src/core/features/customer/landing/components/DealsOfTheDay.tsx
@@ -40,42 +40,45 @@ const DealsOfTheDay = ({
     startIndex: 0,
   };
 
-  if (!dealsOfTheDay) return <LandingCarouselSkeleton />;
+  if (!dealsOfTheDay && loading) return <LandingCarouselSkeleton />;
 
-  return (
-    <div className={`relative ${className}`}>
-      <div className=" flex items-center justify-between mb-6  max-sm:flex max-sm:flex-col max-sm:gap-2 max-sm:items-start">
-        <h3 className="text-2xl font-bold text-primary ">{title}</h3>
-        <Link
-          href={`/store/${store}/search`}
-          className="relative inline-flex items-center gap-2  text-[var(--primary)] font-semibold hover:text-[#1F2937] transition-colors"
+  if (dealsOfTheDay) {
+    console.log("these are the deals of the day", dealsOfTheDay);
+    return (
+      <div className={`relative ${className}`}>
+        <div className=" flex items-center justify-between mb-6  max-sm:flex max-sm:flex-col max-sm:gap-2 max-sm:items-start">
+          <h3 className="text-2xl font-bold text-primary ">{title}</h3>
+          <Link
+            href={`/store/${store}/search`}
+            className="relative inline-flex items-center gap-2  text-[var(--primary)] font-semibold hover:text-[#1F2937] transition-colors"
+          >
+            <span className="relative group">
+              More Products
+              <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[var(--primary)] transition-all duration-300 group-hover:w-full group-hover:bg-[#1F2937]"></span>
+            </span>
+            <ArrowForward className="text-base" />
+          </Link>
+        </div>
+        <div
+          className=" right-0"
+          style={{ width: `${containerWidthPercentage}%` }}
         >
-          <span className="relative group">
-            More Products
-            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[var(--primary)] transition-all duration-300 group-hover:w-full group-hover:bg-[#1F2937]"></span>
-          </span>
-          <ArrowForward className="text-base" />
-        </Link>
+          <SeCarousel options={options}>
+            {dealsOfTheDay.items?.map((product) => (
+              <ProductCard
+                key={product.id}
+                id={product.id}
+                imageSrc={product.images[0]}
+                title={product.name}
+                price={product.price}
+                rating={product.rating}
+              />
+            ))}
+          </SeCarousel>
+        </div>
       </div>
-      <div
-        className=" right-0"
-        style={{ width: `${containerWidthPercentage}%` }}
-      >
-        <SeCarousel options={options}>
-          {dealsOfTheDay.items?.map((product) => (
-            <ProductCard
-              key={product.id}
-              id={product.id}
-              imageSrc={product.images[0]}
-              title={product.name}
-              price={product.price}
-              rating={product.rating}
-            />
-          ))}
-        </SeCarousel>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default DealsOfTheDay;
